d685e9bbf6439b97886250e39aadb1b019826d49
好好住-liyuanwu <liyuanwu@haohaozhu.com>
2023-03-28 19:02:30 +0800

    grpc demo

--- go.mod
+++ go.mod
@@ -1,3 +1,21 @@
 module github.com/liyuanwu2020/goods
 
 go 1.19
+
+require (
+	github.com/golang/protobuf v1.5.3
+	github.com/liyuanwu2020/msgo v0.0.0-20230327153155-14fba44fd435
+	google.golang.org/protobuf v1.28.1
+)
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.11.2 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	golang.org/x/crypto v0.5.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"github.com/liyuanwu2020/goods/api"
 	"github.com/liyuanwu2020/goods/model"
 	"github.com/liyuanwu2020/msgo"
+	"google.golang.org/grpc"
+	"log"
+	"net"
 	"net/http"
 )
 
 func main() {
+
 	engine := msgo.Default()
 	group := engine.Group("goods")
 	group.Get("/find", func(ctx *msgo.Context) {
 		goods := &model.Goods{Name: "一个花瓶", Id: 1008}
 		ctx.JSON(http.StatusOK, &model.Result{Code: 1000, Msg: "success", Data: goods})
 	})
+	listen, _ := net.Listen("tcp", ":9111")
+	server := grpc.NewServer()
+	api.RegisterGoodsApiServer(server, &api.GoodsRpcService{})
+	err := server.Serve(listen)
+	log.Println(err)
+
 	engine.Run(":9002")
 }
